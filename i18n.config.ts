944de1463a39f134d0efcf85033d15d39af2ab82
Thomas Philippot <thomas.philippot@labo-mylab.fr>
2023-06-20 17:14:43 +0200

    first version

--- i18n.config.ts
+++ i18n.config.ts
@@ -23,12 +23,24 @@ export default defineI18nConfig(() => ({
         fr: {
             welcome: 'Bonjour, je m\'appelle Thomas',
             tagline: 'Je suis un développeur web Français de 25 ans.',
-            projects: 'Mes projets'
+            projects: 'Mes projets',
+            experience: 'Expérience',
+            today: "Aujourd'hui",
+            'fullstack-dev': 'Développeur Fullstack',
+            'mylab-exp': "En charge du développement et de la mise en production d'applications interne, " +
+                "je conçois une base de données puis une API REST correspondant aux besoins, puis une interface utilisateur " +
+                "(single page application). Je me charge également de la mise en production sur un serveur NGINX."
         },
         en: {
             welcome: 'Hello my name is Thomas',
             tagline: 'I am a French web developer, 25 years old.',
-            projects: 'My projects'
+            projects: 'My projects',
+            experience: 'Experience',
+            today: 'today',
+            'fullstack-dev': 'Fullstack Developer',
+            'mylab-exp': "In charge of the development and shipping of internal applications, " +
+                "I design a database then a REST API corresponding to the user needs, then a user interface" +
+                "(single page application). I also take care of the app shipping on an NGINX server."
         }
     }
 }))
--- pages/index.vue
+++ pages/index.vue
@@ -36,16 +36,42 @@
       </div>
     </div>
   </section>
-  <section class="py-16">
-      <h2 class="text-4xl font-semibold text-teal mb-5 px-6 md:px-0">{{ $t('projects') }}</h2>
-      <div class="flex flex-col md:flex-row flex-wrap items-center justify-center gap-6">
-          <a class="rounded-md border-2 border-teal w-full md:w-1/4" href="https://mylene-naturo.fr">
+    <section class="py-16 px-6 lg:px-0">
+        <h2 class="text-4xl font-semibold text-flamingo mb-5 px-6 md:px-0">{{ $t('experience') }}</h2>
+        <div class="flex flex-col gap-6">
+            <div class="flex flex-col md:flex-row items-start gap-4 p-4">
+                <p class="text-sm text-subtext0 uppercase mt-1">2018 - {{ $t('today')}}</p>
+                <div class="flex-1">
+                    <h4 class="text-xl font-semibold">{{ $t('fullstack-dev')}}</h4>
+                    <p class="mt-2">Laboratoire MyLab, Chateaugiron</p>
+                    <p class="text-xs font-light mt-2">{{ $t('mylab-exp')}}</p>
+                    <div class="flex flex-wrap gap-2 py-2">
+                        <div class="rounded-full text-xs text-flamingo font-medium leading-5 border border-flamingo px-3 py-1">Nuxt js</div>
+                        <div class="rounded-full text-xs text-flamingo font-medium leading-5 border border-flamingo px-3 py-1">Vue JS</div>
+                        <div class="rounded-full text-xs text-flamingo font-medium leading-5 border border-flamingo px-3 py-1">Vuetify</div>
+                        <div class="rounded-full text-xs text-flamingo font-medium leading-5 border border-flamingo px-3 py-1">PHP Symfony</div>
+                        <div class="rounded-full text-xs text-flamingo font-medium leading-5 border border-flamingo px-3 py-1">GIT</div>
+                        <div class="rounded-full text-xs text-flamingo font-medium leading-5 border border-flamingo px-3 py-1">Docker</div>
+                        <div class="rounded-full text-xs text-flamingo font-medium leading-5 border border-flamingo px-3 py-1">NGINX</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  <section class="py-16 px-6 lg:px-0">
+      <h2 class="text-4xl font-semibold text-teal mb-5">{{ $t('projects') }}</h2>
+      <div class="flex flex-col lg:flex-row flex-wrap items-center justify-center gap-6">
+          <a class="rounded-md border-2 border-teal w-full sm:w-1/2 md:w-1/3 lg:w-1/4" href="https://mylene-naturo.fr">
               <img src="assets/sites/mylene-naturo.png" class="rounded-t-md w-full h-40" />
               <div class="p-5">
                   <div class="text-2xl font-bold text-teal">
                       Mylene Naturo
                   </div>
-                  <p class="my-3 font-normal">Nuxt3, Nuxt Content, TailwindCSS</p>
+                  <div class="flex flex-wrap gap-2 my-3">
+                      <div class="rounded-full text-xs leading-5 border border-teal px-3 py-1">Nuxt 3</div>
+                      <div class="rounded-full text-xs leading-5 border border-teal px-3 py-1">Nuxt Content</div>
+                      <div class="rounded-full text-xs leading-5 border border-teal px-3 py-1">TailwindCSS</div>
+                  </div>
               </div>
           </a>
           <a class="rounded-md border-2 border-teal " href="https://mineral-contest.netlify.app">
@@ -54,7 +80,10 @@
                   <div class="text-2xl font-bold text-teal">
                       Mineral Contest
                   </div>
-                  <p class="my-3 font-normal">Nuxt2, Vuetify</p>
+                  <div class="flex flex-wrap gap-2 my-3">
+                      <div class="rounded-full text-xs leading-5 border border-teal px-3 py-1">Nuxt 2</div>
+                      <div class="rounded-full text-xs leading-5 border border-teal px-3 py-1">Vuetify</div>
+                  </div>
               </div>
           </a>
       </div>
