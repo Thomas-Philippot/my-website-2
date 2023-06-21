a3d03646542ab7cd8163412d1dca7414bdfed5a6
Thomas Philippot <thomas.philippot@labo-mylab.fr>
2023-06-21 12:00:48 +0200

    margin and center issues

--- app.vue
+++ app.vue
@@ -2,23 +2,21 @@
     <body :class="$colorMode.value" class="bg-base text-text min-h-screen">
     <div class="container mx-auto max-w-[1300px]">
         <nav class="w-full h-10 py-10 px-2">
-            <div class="flex justify-end items-center">
-                <ul class="flex">
-                    <li>
-                        <select v-model="locale" class="mx-6 bg-base font-bold text-xl focus:outline-none">
-                            <option value="fr">
-                                🇫🇷 Français
-                            </option>
-                            <option value="en">
-                                🇬🇧 English
-                            </option>
-                        </select>
-                    </li>
-                    <li class="mx-6 font-bold text-xl">
-                        <theme-switcher/>
-                    </li>
-                </ul>
-            </div>
+          <ul class="flex justify-end items-center">
+            <li>
+              <select v-model="locale" class="mx-6 bg-base font-bold text-xl focus:outline-none">
+                <option value="fr">
+                  🇫🇷 Français
+                </option>
+                <option value="en">
+                  🇬🇧 English
+                </option>
+              </select>
+            </li>
+            <li class="mx-6 font-bold text-xl">
+              <theme-switcher/>
+            </li>
+          </ul>
         </nav>
         <NuxtPage/>
     </div>
