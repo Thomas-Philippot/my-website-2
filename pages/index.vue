96f17dba033c7e37d094583b236cd644de1515e2
Thomas Philippot <thomas.philippot@labo-mylab.fr>
2023-06-22 10:43:09 +0200

    image compression

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <section class="w-full flex items-center justify-between pb-16 sm:pb-4 flex-col md:flex-row-reverse xs:px-5">
     <div class="mt-6 px-6 md:px-12 md:px-12 shrink-0">
       <img
-          src="/assets/avatar.png"
+          src="/assets/avatar.webp"
           alt="Thomas Profile avatar"
           class="rounded-full w-[200px] h-[200px] sm:w-[400px] sm:h-[400px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px]"
       >
@@ -62,7 +62,7 @@
       <h2 class="text-4xl font-semibold text-teal mb-5">{{ $t('projects') }}</h2>
       <div class="flex flex-col lg:flex-row flex-wrap items-center justify-center gap-6">
           <a class="rounded-md border-2 border-teal w-full sm:w-1/2 md:w-1/3 lg:w-1/4" href="https://mylene-naturo.fr">
-              <img src="/assets/sites/mylene-naturo.png" class="rounded-t-md w-full h-40" />
+              <img src="/assets/sites/mylene-naturo.webp" class="rounded-t-md w-full h-40" />
               <div class="p-5">
                   <div class="text-2xl font-bold text-teal">
                       Mylene Naturo
@@ -75,7 +75,7 @@
               </div>
           </a>
           <a class="rounded-md border-2 border-teal w-full sm:w-1/2 md:w-1/3 lg:w-1/4" href="https://mineral-contest.netlify.app">
-              <img src="/assets/sites/mineral-contest.png" class="rounded-t-md w-full h-40" />
+              <img src="/assets/sites/mineral-contest.webp" class="rounded-t-md w-full h-40" />
               <div class="p-5">
                   <div class="text-2xl font-bold text-teal">
                       Mineral Contest
